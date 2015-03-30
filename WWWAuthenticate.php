7069ced5551cd10cd306781156b39362a97710ae
Marco <mchang@mvsusa.com>
2015-03-30 02:15:57 -0400

    Added WWWAuthenticate

---
+++ WWWAuthenticate.php
@@ -0,0 +1,29 @@
+<?php
+require_once('HttpHeader.php');
+
+Class WWWAuthenticate implements HTTPHeader {
+
+	public $authenticate;
+	
+	public function __construct($type = 'basic', $send = false, Array $other = null) {
+		$this->authenticate = $type;
+
+		if( ! is_null($other)) {
+			$temp = array();
+			
+			foreach($other as $key => $val) {
+				array_push($temp, "{$key}={$val}");
+			}
+
+			$this->authenticate .= ' '.implode(', ', $temp);
+		}
+
+		if($send) {
+			$this->sendHeader();
+		}
+	}
+
+	public function sendHeader() {
+		return header("WWW-Authenticate: {$this->authenticate}");
+	}
+}
