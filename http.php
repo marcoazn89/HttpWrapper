a9fa2275bba3150524bbba6701233172185c78cd
Marco <mchang@mvsusa.com>
2015-03-30 11:22:11 -0400

    Syncing files

--- http.php
+++ http.php
@@ -1,5 +1,9 @@
 <?php
 require_once('Status.php');
+require_once('ContentType.php');
+require_once('Language.php');
+require_once('WWWAuthenticate.php');
+require_once('CacheControl.php');
 
 Class HTTP {
 	private static $headers = array();
@@ -13,12 +17,45 @@ Class HTTP {
 		}
 	}
 
+	public static function contentType($contentType, $init = false) {
+		if($init) {
+			return new ContentType($contentType, $init);
+		}
+		else {
+			array_push(self::$headers, new ContentType($contentType, $init));
+		}
+	}
+
+	public static function language($language, $init = false) {
+		if($init) {
+			return new ContentType($language, $init);
+		}
+		else {
+			array_push(self::$headers, new Language($language, $init));
+		}
+	}
+
+	public static function authenticate($type = 'basic', Array $other = null, $init = false) {
+		if($init) {
+			return new WWWAuthenticate($type, $init, $other);
+		}
+		else {
+			array_push(self::$headers, new WWWAuthenticate($type, $other, $init));
+		}
+	}
+
+	public static function cache(CacheControl $cache, $init = false) {
+		if($init) {
+			return $cache->sendHeader();
+		}
+		else {
+			array_push(self::$headers, $cache);
+		}
+	}
+
 	public static function sendResponse() {
 		foreach(self::$headers as $header) {
 			$header->sendHeader();
 		}
 	}
 }
-
-HTTP::status(300);
-HTTP::sendResponse();
