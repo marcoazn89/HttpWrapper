ce03c97c8c0e22690f5ebc5706161802791e367f
Marco Chang <imnotmarco@live.com>
2015-07-09 12:16:05 -0400

    Adding CORS headers

--- src/Response/ResponseHeaders.php
+++ src/Response/ResponseHeaders.php
@@ -7,4 +7,7 @@ interface ResponseHeaders {
 	const CONTENT_LENGTH = 'Content-Length';
 	const LANGUAGE = 'Language';
 	const ALLOW = 'Allow';
+	const ACCESS_CONTROL_ALLOW_METHODS = 'Access-Control-Allow-Methods';
+	const ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin';
+	const ACCESS_CONTROL_ALLOW_HEADERS = 'Access-Control-Allow-Headers';
 }
